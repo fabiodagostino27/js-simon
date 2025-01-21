dfc789b8122268a6734be8003381daf8d3408855
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-21 16:35:00 +0100

    Exercise done

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 let countdown = document.getElementById("countdown");
-let counter = 1;
+let counter = 30;
 let numbersList = document.getElementById("numbers-list");
 let randomArr = [];
 let answersForm = document.getElementById("answers-form");
 let numbers = document.getElementsByClassName("form-control");
 let btn = document.querySelector(".btn");
-let message = document.getElementById("message")
+let message = document.getElementById("message");
 let rightAnswers = 0;
-let rightArr = []
+let rightArr = [];
 
 function randomNumberGroup(arr) {
     for (let i = 0; i <= 4; i++) {
         arr.push(Math.floor(Math.random() * 50) + 1);
     }
 
-    return arr
+    return arr;
 }
 
 
-numbersList.innerHTML = randomNumberGroup(randomArr)
+numbersList.innerHTML = randomNumberGroup(randomArr);
 
 
 let interval = setInterval(function(){
@@ -28,22 +28,31 @@ let interval = setInterval(function(){
     if (counter == 0) {
         clearInterval(interval);
         numbersList.classList.replace("d-flex", "d-none");
-        answersForm.classList.replace("d-none", "d-block")
+        answersForm.classList.replace("d-none", "d-block");
     }
 }, 1000)
 
 btn.addEventListener("click", function(event) {
     event.preventDefault();
-    
+    if (btn.innerHTML == "Conferma") {
+        btn.innerHTML = "Ricomincia!";
 
-    for (i = 0; i < randomArr.length; i++) {
-        for (k = 0; k < randomArr.length; k++) {
-            if (numbers[i].value == randomArr[k]) {
-                rightAnswers++;
-                rightArr.push(numbers[i].value)
+        for (i = 0; i < randomArr.length; i++) {
+            for (k = 0; k < randomArr.length; k++) {
+                if (numbers[i].value == randomArr[k]) {
+                    rightAnswers++;
+                    rightArr.push(numbers[i].value);
+                }
             }
         }
+    
+        if (rightAnswers > 0) {
+            message.innerHTML = `Hai indovinato ${rightAnswers} numeri! (${rightArr})`;
+            message.classList.replace("text-danger", "text-success");
+        } else {
+            message.innerHTML = "Non hai indovinato nessun numero!";
+        }
+    } else {
+        location.reload();
     }
-
-    message.innerHTML = rightArr
 })
