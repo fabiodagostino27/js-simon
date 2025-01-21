699dee0f47e85d6a99ff98c658a2efd9d6a4ce68
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-21 14:57:29 +0100

    ADD: random array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 let countdown = document.getElementById("countdown");
 let counter = 30;
+let numbersList = document.getElementById("numbers-list");
+let randomArr = [];
+
+function randomNumberGroup(arr) {
+    for (let i = 0; i <= 4; i++) {
+        arr.push(Math.floor(Math.random() * 50) + 1);
+    }
+
+    return arr
+}
+
+
+numbersList.innerHTML = randomNumberGroup(randomArr)
 
 let interval = setInterval(function(){
     counter--;
